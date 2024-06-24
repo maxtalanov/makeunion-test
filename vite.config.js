53dcb11f573c92d1c82dc76a2a011fae476ee4c9
Maxim Talanov <mail@maxtalanov.ru>
2024-06-25 02:57:34 +0300

    feat(alias): Добавлены и настроены alias'ы для упрощения импортов
    - components
    - ui
    - app
    - utils
    - assets
    - pages
    - api
    - hook

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components/modules'),
+      '@app': path.resolve(__dirname, './src/components/App/App.jsx'),
+      '@ui': path.resolve(__dirname, './src/components/UI'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@hook': path.resolve(__dirname, './src/pages'),
+    }
+  }
 })
